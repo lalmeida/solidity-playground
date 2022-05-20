fd7db49639c4f137f833a351ba8ebf3d8e2df2c8
"Leonardo" <"leonardo.fig.almeida@gmail.com">
2022-05-20 19:15:01 -0300

    Refactoring and Withdraw implementation

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -29,15 +29,26 @@ contract Bank {
      * User withdraws money from their account.
      */
     function withdraw (uint amount) public {
-        require (accounts[msg.sender].balance >= amount);
+        require (amount > 0, "Cannot withdraw a zero amount.");
+        require (accounts[msg.sender].balance >= amount, "User does not have enough balance");
         accounts[msg.sender].balance -= amount;
+        payable(msg.sender).transfer(amount);
         emit balanceUpdated(msg.sender, accounts[msg.sender].balance);
     }
 
-    /** 
-     * User requests its Balance.
+     /** 
+     * User requests its own Balance.
      */
-    function getBalance(address userAddress) public view returns (uint) {
+    function getBalance() public view returns (uint) {
+        return getBalanceOf(msg.sender);
+    }
+
+
+    /** 
+     * Requests Balance of any account.
+     * (Should be restricted in production or made private)
+     */
+    function getBalanceOf(address userAddress) public view returns (uint) {
         return accounts[userAddress].balance;
     }
 
