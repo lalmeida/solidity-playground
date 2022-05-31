d7155b32413dd3be09dbada5ab5a7f74eead8237
"Leonardo" <"leonardo.fig.almeida@gmail.com">
2022-05-30 21:19:04 -0300

    Gas usage tests using MappingStorage

---
+++ contracts/MappingStorage.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract MappingStorage {
+
+    struct Entity{
+        uint myData;
+        address myAddress;
+    }
+
+    mapping (address => Entity) mappingStorage;
+
+    function addEntity(uint _data, address _address) public {
+        mappingStorage[msg.sender] = Entity (_data, _address);
+    }
+
+    function updateEntity(uint _data, address _address) public {
+        mappingStorage[msg.sender].myData = _data;
+        mappingStorage[msg.sender].myAddress = _address;
+
+    }   
+
+
+}
