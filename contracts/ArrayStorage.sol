882b3e87403e2b87628deb4a3a5eead6bef8d884
"Leonardo" <"leonardo.fig.almeida@gmail.com">
2022-05-31 00:09:48 -0300

    Gas usage tests using Mapping and Arrays for Storage

--- contracts/ArrayStorage.sol
+++ contracts/ArrayStorage.sol
@@ -13,10 +13,16 @@ contract ArrayStorage {
 
     Entity[] arrayStorage;
 
+    /**
+     * execution cost	88190/71102/71114/71090  gas 
+     */
     function addEntity(uint myData) public {
         arrayStorage.push( Entity(myData, msg.sender) );
     }
 
+     /**
+     * execution cost   52489 /  55289 / 55289 gas
+     */
     function updateEntity(uint myData) public {
         
         for (uint i=0 ; i < arrayStorage.length ; i++) {
@@ -27,8 +33,12 @@ contract ArrayStorage {
 
     }  
 
+     /**
+     * execution cost (w/ require)  31523 / 31511 / 31535  gas
+     * (w/o require): 26344  / 29156 / 29156 gas
+     */
     function updateEntity(uint index, uint myData) public {
-        require (arrayStorage[index].myAddress == msg.sender, "Caller cannot update this index.");
+        //require (arrayStorage[index].myAddress == msg.sender, "Caller cannot update this index.");
         arrayStorage[index].myData = myData;
     }  
 
--- contracts/MappingStorage.sol
+++ contracts/MappingStorage.sol
@@ -13,10 +13,18 @@ contract MappingStorage {
 
     mapping (address => Entity) mappingStorage;
 
+    /**
+     * execution cost	66054 / 66090 / 66114 gas
+     * further executions for same msg.sender (update): 29066 / 29078 gas (cost similar to update function) 
+     */
     function addEntity(uint _data) public {
         mappingStorage[msg.sender] = Entity (_data, msg.sender);
     }
 
+    /**
+     * execution cost  26719 / 23931 / 23943 gas
+     *  (for new msg.senders)    43831 
+     */
     function updateEntity(uint _data) public {
         mappingStorage[msg.sender].myData = _data;
     }   
